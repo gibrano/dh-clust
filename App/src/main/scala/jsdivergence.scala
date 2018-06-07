ebe4ae4000880e226455861c8dd98c55b029dcba
Gibran Otazo <gibran@entropy.tech>
2018-06-07 09:49:20 -0500

    Update jsdivergence.scala

--- App/src/main/scala/jsdivergence.scala
+++ App/src/main/scala/jsdivergence.scala
@@ -3,27 +3,28 @@ package dhclust
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.mllib.linalg.Vectors
 
-object Divergence {
+object Divergence extends Serializable {
 
-  def JensenShannon(A: Array[org.apache.spark.mllib.linalg.Vector], B: Array[org.apache.spark.mllib.linalg.Vector], sc: SparkContext): Double = {
-     var n = A.size - 1
-     var C = Array[org.apache.spark.mllib.linalg.Vector]()
-     for(i <- 0 to n){
-        var x = Vectors.zeros(n+1)
-        for(j <- 0 to n){
-           x.toArray(j) = 0.5*(A(i).toArray(j) - B(i).toArray(j))
-        }
-        C = C ++ Array(x)
+  def JensenShannon(A: Array[Array[Double]], B: Array[Array[Double]]): Double = {
+     var C = Array[Array[Double]]()
+     var n = A.size
+     for(i <- 0 to (n-1)){
+       var x = Array.fill(n)(0.00)
+       for(j <- 0 to (n-1)){
+         x(j) = 0.5*(A(i)(j)+B(i)(j))
+         if(x(j) > 1){
+           x(j) = 1
+         }
+       }
+       C = C ++ Array(x)
      }
-     var r = Entropy.VonNewmann(C, sc)-(1/2)*(Entropy.VonNewmann(A, sc)+Entropy.VonNewmann(B, sc))
+     var r = Entropy.VonNewmann(C)-(1/2)*(Entropy.VonNewmann(A)+Entropy.VonNewmann(B))
      return r
   }
 
-  def computeJSD(x: Array[Int], layers: Array[Array[org.apache.spark.mllib.linalg.Vector]], sc: SparkContext) : Double = {
-    var jsd = JensenShannon(layers(x(0)),layers(x(1)), sc)
+  def computeJSD(x: Array[Int], layers: Array[Array[Array[Double]]]) : Double = {
+    var jsd = JensenShannon(layers(x(0)),layers(x(1)))
     return jsd
   }
 
