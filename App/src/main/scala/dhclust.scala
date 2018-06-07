f814b44a69c33ed3bc0b353ea70dc3dadaabce6b
Gibran Otazo <gibran@entropy.tech>
2018-06-07 09:47:24 -0500

    Update dhclust.scala

--- App/src/main/scala/dhclust.scala
+++ App/src/main/scala/dhclust.scala
@@ -4,60 +4,52 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-object Clusters {
+object Clusters extends Serializable {
 
-  def Hierarchical(C: Array[Array[org.apache.spark.mllib.linalg.Vector]], sc: SparkContext): Array[Array[Int]] = {
-    var layers = C
-    var n = layers.size - 1
+  def Hierarchical(layers: Array[Array[Array[Double]]], sc: SparkContext): Array[Array[Double]] = {
+    var C = layers
+    var linkages = Array[Array[Double]]()
+    var l = C.size
+    var m = C(0).size
+    
     var A = layers(0)
-    for(i <- 1 to n){
-       A = Graph.aggregate(A,layers(i))
+    for(i <- 1 to (l-1)){
+       A = Graph.aggregate(A,C(i))
     }
-    val hA = Entropy.VonNewmann(A, sc)
-    var clusters = sc.parallelize(0 to n).map(i => Array(i)).collect
-    var aux = clusters
-
-    var globalquality = Entropy.GlobalQuality(layers, hA, sc)
-    println(globalquality)
-    var max = globalquality
+    
+    val hA = Entropy.VonNewmann(A)
+        
+    var globalquality = Entropy.GlobalQuality(C, hA)
     var q = Array[Double](globalquality)
 
-    while(layers.size > 1){
-       var n = layers.size
-       var coords = Array[Array[Int]](Array[Int]())
-       for( i <- 0 to n-2){
-          for(j <- i+1 to n-1){
-             coords = coords ++ Array(Array(i,j))
-          }
-       }
-       coords = coords.filter(_.size > 0)
-
-       var jsdMatrix = coords.map(x => Divergence.computeJSD(x, layers, sc))
-       var minimum = jsdMatrix.zipWithIndex.min
-       var a = coords(minimum._2)(0)
-       var b = coords(minimum._2)(1)
-       var Cx = layers(a)
-       var Cy = layers(b)
-       var newlayer = Graph.aggregate(Cx,Cy)
-       layers = layers.filter(_ != Cx)
-       layers = layers.filter(_ != Cy)
-       layers = layers ++ Array(newlayer)
-
-       var v1 = aux(a)
-       var v2 = aux(b)
-       aux = aux.filter(_ != v1)
-       aux = aux.filter(_ != v2)
-       aux = aux.union(Array(v1.union(v2)))
-
-       globalquality = Entropy.GlobalQuality(layers, hA, sc)
-       println(globalquality)
-
-       if(globalquality >= max){
-         max = globalquality
-         clusters = aux
-       }
-       q = q ++ Array(globalquality)
+    while(C.size > 1){
+      var n = C.size
+      println("Layers size", n)
+      var coords = Array[Array[Int]](Array[Int]())
+      for( i <- 0 to n-2){
+        for(j <- i+1 to n-1){
+          coords = coords ++ Array(Array(i,j))
+        }
+      }
+      coords = coords.filter(_.size > 0)
+      var t2 = System.nanoTime
+      var jsdMatrix = sc.parallelize(coords).map(x => Divergence.computeJSD(x, C))
+      var duration2 = (System.nanoTime - t2) / 1e9d
+      println("Duration time div JS:",duration2)
+      var minimum = jsdMatrix.zipWithIndex.min
+      var a = coords(minimum._2.toInt)(0)
+      var b = coords(minimum._2.toInt)(1)
+      var Cx = C(a)
+      var Cy = C(b)
+      var newlayer = Graph.aggregate(Cx,Cy)
+      C = C.filter(_ != Cx)
+      C = C.filter(_ != Cy)
+      C = C ++ Array(newlayer)
+      globalquality = Entropy.GlobalQuality(C, hA)
+      println("Global quality:",globalquality)
+      q = q ++ Array(globalquality)
+      linkages = linkages ++ Array(Array(a,b,globalquality))
     }
-    return clusters
+    return linkages
   }
 }
