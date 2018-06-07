c4c29c33b55f8ebec80df80f80170a6253e64315
Gibran Otazo <gibran@entropy.tech>
2018-06-07 09:50:00 -0500

    Update tdm.scala

--- App/src/main/scala/tdm.scala
+++ App/src/main/scala/tdm.scala
@@ -3,30 +3,22 @@ package dhclust
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.linalg.distributed.RowMatrix
 
-object TM {
-
-  def termDocumentMatrix(texts: Array[String], sc: SparkContext): Array[org.apache.spark.mllib.linalg.Vector] = {
-    var corpus = sc.parallelize(texts)
-    var tokens = corpus.map(x => x.split(" "))
+object TM extends Serializable {
+  def termDocumentMatrix(texts: org.apache.spark.rdd.RDD[String], sc: SparkContext): Array[Array[Double]] = {
+    var tokens = texts.map(x => x.split(" "))
     var dwords = sc.broadcast(tokens.flatMap(token => token).distinct().collect)
     val n = dwords.value.size
-    var tdm = Array[org.apache.spark.mllib.linalg.Vector]()
-
+    var tdm = Array[Array[Double]]()
     var documents = sc.broadcast(tokens.collect)
-    for( i <- documents.value){
-       var x = Vectors.zeros(n)
-       for( j <- i){
+    for(i <- documents.value){
+      var x = Array.fill(n)(0.00)
+       for(j <- i){
          var index = dwords.value.indexOf(j)
-         x.toArray(index) = 1
+         x(index) = 1.00
        }
        tdm = tdm ++ Array(x)
     }
-
     return tdm
   }
-
 }
